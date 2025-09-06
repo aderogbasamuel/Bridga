1ec86939e099e4cb4696f7a84054c7889eb44187
aderogbasamuel <aderogbasamueladeolu@gmail.com>
2025-09-06 23:19:21 +0100

    update

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -11,7 +11,7 @@ function HomePage() {
             </div>
             <section className='px-10 py-10'>
                 <h3 className='heading text-[16px] uppercase font-bold mb-12'>Featured Categories</h3>
-                <div className='grid grid-cols-2 sm:grid-cols-3 md:grd-cols-3 gap-6'>
+                <div className='grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4  gap-6'>
                     {[
                         {
                             image: image1,
