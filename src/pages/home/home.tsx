b197f6c3c55d11d5d446d5713f9b1b2991a7139e
aderogbasamuel <aderogbasamueladeolu@gmail.com>
2025-09-07 01:15:00 +0100

    fixed responsiveness issue in home page

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -44,8 +44,8 @@ function HomePage() {
                     }
                 </div>
             </section>
-            <section className='px-4 sm:px-4 md:px-24  py-10'>
-                <h3 className='text-gray-800 full-heading text-[20px] uppercase font-bold'>Featured Products</h3>
+            <section className='px-0 sm:px-0  md:px-24  py-10'>
+                <h3 className='text-gray-800 mx-4 sm:mx-4 full-heading text-[20px] uppercase font-bold'>Featured Products</h3>
                 <div className='grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mt-6 sm:mt-12'>
                     {[
                         {
