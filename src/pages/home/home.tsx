de10706fefeba894603e4e5885b4442391ca660a
aderogbasamuel <aderogbasamueladeolu@gmail.com>
2025-09-07 01:11:39 +0100

    added and removed gap

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -11,7 +11,7 @@ function HomePage() {
             </div>
             <section className=' px-4 sm:px-4 md:px-24  py-10'>
                 <h3 className='heading text-[16px] uppercase font-bold mb-12'>Featured Categories</h3>
-                <div className='grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
+                <div className='grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6'>
                     {[
                         {
                             image: image1,
@@ -46,7 +46,7 @@ function HomePage() {
             </section>
             <section className='px-4 sm:px-4 md:px-24  py-10'>
                 <h3 className='text-gray-800 full-heading text-[20px] uppercase font-bold'>Featured Products</h3>
-                <div className='grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4  gap-6 mt-6 sm:mt-12'>
+                <div className='grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mt-6 sm:mt-12'>
                     {[
                         {
                             image: image1,
