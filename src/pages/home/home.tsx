b9fdc0a6cebebd9e3d0f43ac1d6f5a7640a25667
aderogbasamuel <aderogbasamueladeolu@gmail.com>
2025-09-07 01:10:25 +0100

    renmoved gap

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -11,7 +11,7 @@ function HomePage() {
             </div>
             <section className=' px-4 sm:px-4 md:px-24  py-10'>
                 <h3 className='heading text-[16px] uppercase font-bold mb-12'>Featured Categories</h3>
-                <div className='grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4  gap-6'>
+                <div className='grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
                     {[
                         {
                             image: image1,
